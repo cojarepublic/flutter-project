f3d7c7c9555d3c1bdb415a547fc6326c14fa9254
cojarepublic <60820059+cojarepublic@users.noreply.github.com>
2020-05-12 19:41:24 +0100

    This is a simple flutter contact card

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,97 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.indigo.shade500,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/passport photo.jpg'),
+              ),
+              Text(
+                'Chukwujioke Obinna',
+                style: TextStyle(
+                  fontFamily: 'Righteous',
+                  fontSize: 27.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Co-Founder Wirepay',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.indigo.shade100,
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 130.0,
+                child: Divider(
+                  color: Colors.indigo.shade100,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.phone,
+                      color: Colors.indigo.shade900,),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text('0803 123 4567',
+                      style: TextStyle(
+                        color: Colors.indigo.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                      ),)
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.email,
+                      color: Colors.indigo.shade900,),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text('mailme@gmail.com',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontFamily: 'Source Sans Pro',
+                        color: Colors.indigo.shade900,
+                      ),)
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
